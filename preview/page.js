576f792d67f6c2dad0495f71e0596db5ae321d8a
ByteLight Plugin Repositories <plugins@bytelight.com>
2014-11-13 11:58:23 +0000

    Released on: 2014-11-13 11:58:19 +0000

---
+++ preview/page.js
@@ -0,0 +1,19 @@
+$('.item-milk').on('click', function() {
+   BL.MAP.wayfindTo(BL.MAP.zoneInfo[128].center);
+   return false;
+})
+
+$('.item-crisps').on('click', function() {
+   BL.MAP.wayfindTo(BL.MAP.zoneInfo[133].center);
+   return false;
+})
+
+$('.item-butter').on('click', function() {
+   BL.MAP.wayfindTo(BL.MAP.zoneInfo[148].center);
+   return false;
+})
+
+$('.item-hair-care').on('click', function() {
+   BL.MAP.wayfindTo(BL.MAP.zoneInfo[144].center);
+   return false;
+})
